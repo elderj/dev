9191f4126542f47ff94014c04e105ec68f7cc9ad
elderj <jelde010@gmail.com>
2019-05-18 23:02:55 -0400

    Updates

---
+++ precache-manifest.b1f20ffab0a85e7751d3581743507e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9d98e3feb331a8429521703e30ac1a",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "34ddc5e3901aa22bc0ba",
+    "url": "/dev/static/css/main.05d5c309.chunk.css"
+  },
+  {
+    "revision": "88ee071bf46fc6b893e6",
+    "url": "/dev/static/js/2.7ad91dd1.chunk.js"
+  },
+  {
+    "revision": "34ddc5e3901aa22bc0ba",
+    "url": "/dev/static/js/main.34f50d0b.chunk.js"
+  },
+  {
+    "revision": "7b18ca907f92e9a66def",
+    "url": "/dev/static/js/runtime~main.c0cef5d3.js"
+  },
+  {
+    "revision": "d35f4a25085e37219faf015fad06baeb",
+    "url": "/dev/static/media/dark_wood.d35f4a25.png"
+  }
+]);
