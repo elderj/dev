7e95017ea6afbc63f0371b7745232ac5058b4f32
elderj <jelde010@gmail.com>
2020-11-19 17:27:39 -0500

    Removal of linkedin and github icons from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,11 @@ import React, { Fragment } from "react";
 import "../styles/App.scss";
 import "../styles/font.css";
 
-import GitHubLogo from "../images/linkLogoImages/GitHub-Mark-Light-120px-plus.png";
-import LinkedInLogo from "../images/linkLogoImages/In-White-72.png";
+// import GitHubLogo from "../images/linkLogoImages/GitHub-Mark-Light-120px-plus.png";
+// import LinkedInLogo from "../images/linkLogoImages/In-White-72.png";
 
 const Footer = () => (
   <Fragment>
-    <a href="https://github.com/elderj">
-      <img className="linkLogoImage" alt="Github logo" src={GitHubLogo} />
-    </a>
-    <a href="https://www.linkedin.com/in/elderj/">
-      <img className="linkLogoImage" alt="LinkedIn logo" src={LinkedInLogo} />
-    </a>
 
     <br />
 
