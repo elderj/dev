0f65b0df7431b9b61ba3367758fd1ef9595c8c2a
elderj <jelde010@gmail.com>
2021-09-04 02:33:15 -0400

    Updates

---
+++ precache-manifest.9c0234895177f0e182fc608bb859c087.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d10f6a4d1c0c2d473bf9a510255ffc",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "e8aba4c327b98328356e",
+    "url": "/dev/static/css/main.b8f8e0bb.chunk.css"
+  },
+  {
+    "revision": "698b9f07e18a23d0bc82",
+    "url": "/dev/static/js/2.b8e04991.chunk.js"
+  },
+  {
+    "revision": "e8aba4c327b98328356e",
+    "url": "/dev/static/js/main.15352d51.chunk.js"
+  },
+  {
+    "revision": "7b18ca907f92e9a66def",
+    "url": "/dev/static/js/runtime~main.c0cef5d3.js"
+  },
+  {
+    "revision": "d35f4a25085e37219faf015fad06baeb",
+    "url": "/dev/static/media/dark_wood.d35f4a25.png"
+  },
+  {
+    "revision": "f669777351b71cd0fb244b88d4975a5f",
+    "url": "/dev/static/media/instagram-logo.f6697773.png"
+  },
+  {
+    "revision": "5ddd0d044581bd8f5326e66951943fab",
+    "url": "/dev/static/media/sc.5ddd0d04.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99e065780161749ff05751ad190f9591.js"
+  "/dev/precache-manifest.9c0234895177f0e182fc608bb859c087.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dev/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
