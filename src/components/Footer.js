edfbbc8a102bf1ae761bb48db95dccf92c41db7f
elderj <jelde010@gmail.com>
2022-05-08 21:42:54 -0400

    Update footer for site, add twitter and update the alts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,17 @@ import "../styles/App.scss";
 import "../styles/font.css";
 
 import instagramLogo from "../images/linkLogoImages/instagram-logo.png";
+import twitterLogo from "../images/linkLogoImages/twit.png";
 
 const Footer = () => (
   <Fragment>
-    <a href="https://www.instagram.com/elderdesignconcepts/">
-      <img className="linkLogoImage" alt="LinkedIn logo" src={instagramLogo} />
+    <a href="https://twitter.com/ElderDesignCo">
+      <img className="linkLogoImage" alt="Twitter logo" src={twitterLogo} />
     </a>
+    <a href="https://www.instagram.com/elderdesignconcepts/">
+      <img className="linkLogoImage" alt="Instagram Logo" src={instagramLogo} />
+    </a>
+
     <br />
     <h6 className="quicksand-text">© 2022 Elder Design Concepts</h6>
   </Fragment>
