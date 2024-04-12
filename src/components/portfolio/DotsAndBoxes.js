dee5829b26b08d20b9106d0b5f2106fd2db7e53b
Joseph Elder <jelde010@gmail.com>
2024-04-12 15:05:20 -0400

    Add link to Dots and Boxes app in store

--- src/components/portfolio/DotsAndBoxes.js
+++ src/components/portfolio/DotsAndBoxes.js
@@ -18,9 +18,9 @@ const DotsAndBoxes = () => (
 
     <p className="quicksand-text blurb">
       Check out the{" "}
-      {/* <a href="">
+      <a href="https://play.google.com/store/apps/details?id=com.elderdesignconcepts.dotsandboxes">
         Android app
-      </a> */}
+      </a>
       <br />
       <ul>
         <li>
